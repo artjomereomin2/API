c0384f83d6df7bd19c6164b84207563981618ba9
Admin <exvita@yandex.ru>
2022-01-31 18:14:42 +0400

    functions without order

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
-SCREEN_SIZE = [350,450]
+SCREEN_SIZE = [350, 450]
 
 import requests
 
@@ -14,29 +14,29 @@ import requests
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.spn_x, self.spn_y = 10,10
+        self.spn_x, self.spn_y = 10, 10
         self.initUI()
 
-    def getImage(self):
-        api_server = "http://static-maps.yandex.ru/1.x/"
-        print(123)
-
+    def teleport(self):
         lat = self.wight.text()
         lon = self.longitude.text()
 
-        print(lon,lat)
+        print(lon, lat)
 
         if not lat:
-            lat = 55.703118
+            self.lat = self.lat
         else:
-            lat = float(lat)
+            self.lat = float(lat)
         if not lon:
-            lon = 7.530887
+            self.lon = self.lon
         else:
-            lon = float(lon)
+            self.lon = float(lon)
 
+        self.getImage(self.lat, self.lon)
 
-        print(lon,lat)
+    def getImage(self, lat, lon):
+        api_server = "http://static-maps.yandex.ru/1.x/"
+        print(123)
 
         params = {
             "ll": ",".join([str(lon), str(lat)]),
@@ -59,24 +59,50 @@ class Example(QMainWindow):
         self.image.setPixmap(pixmap)
 
     def initUI(self):
+        self.lat, self.lon = 55.703118, 7.530887
+
         super().__init__()
         uic.loadUi('01.ui', self)
 
         self.image = QLabel(self)
 
-        self.getImage()
+        self.teleport()
 
         pixmap = QPixmap(self.map_file)
 
         self.image.move(0, 200)
-        self.image.resize(SCREEN_SIZE[0],SCREEN_SIZE[1]-200)
+        self.image.resize(SCREEN_SIZE[0], SCREEN_SIZE[1] - 200)
         self.image.setPixmap(pixmap)
 
-        self.button_to_teleport.clicked.connect(self.getImage)
+        self.button_to_teleport.clicked.connect(self.teleport)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+    def increase(self):
+        self.spn_x *= 1.01
+        self.spn_y *= 1.01
+
+    def decrease(self):
+        self.spn_x //= 1.01
+        self.spn_y //= 1.01
+
+    def moveUp(self):
+        self.lat += self.spn_x
+        self.getImage(self.lat, self.lon)
+
+    def moveDown(self):
+        self.lat -= self.spn_x
+        self.getImage(self.lat, self.lon)
+
+    def moveRight(self):
+        self.lon += self.spn_y
+        self.getImage(self.lat, self.lon)
+
+    def moveLeft(self):
+        self.lon -= self.spn_y
+        self.getImage(self.lat, self.lon)
 
 
 if __name__ == '__main__':
